a89a235b1c90e09d0f66f7fbf1a75504114828de
Luiz Brito <lguilherme.esperanca@gmail.com>
2025-11-03 23:05:03 -0300

    criação da atividade 1

---
+++ aulas-remotas-ds/atividade1.py
@@ -0,0 +1,9 @@
+print('***** VERIFICADOR DE MAIORIDADE *****')
+
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+
+if idade>=18:
+    print(f'Olá {nome}, você é maior de idade.')
+else:
+    print(f'Olá {nome}, você é menor de idade.')
